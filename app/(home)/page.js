01cb27ddad28c11829f1b7155005d628aa88cfca
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-05-28 20:09:27 +0330

    get data

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -1,13 +1,15 @@
-export default function Home() {
-    return (
-      <>
-        <div
-          className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h4 className="fw-bold">داشبورد</h4>
-        </div>
-  
-        <div id="chartdiv"></div>
-      </>
-    );
-  }
-  
+import { getFetch } from "@/utils/fetch";
+
+export default async function Home() {
+  const dataChart = await getFetch("/transactions/chart");
+  console.log(dataChart);
+  return (
+    <>
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h4 className="fw-bold">داشبورد</h4>
+      </div>
+
+      <div id="chartdiv"></div>
+    </>
+  );
+}
