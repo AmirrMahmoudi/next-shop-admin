19847b1de1c402ed68ccf19233a4d84c03cdb380
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-06-02 13:52:03 +0330

    Receive and display products

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: `${process.env.API_PROTOCOL}`,
+        hostname: `${process.env.API_HOSTNAME}`,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
